24421a4ea2e5f4cebf6e8081cb9d627d4a38a5cf
Hakus <archeid@gmail.com>
2014-11-30 23:45:06 -0800

    Now it runs forever

--- temp.rb
+++ temp.rb
@@ -17,62 +17,65 @@ client.connect(networkIP, $port)
 puts "Are you sending or receiving? (Input 0 for send, 1 for receive)"
 option = gets.chomp
 
-if(option.to_i == 0)
-    puts "Where do you want to send?"
-    ip = gets.chomp
 
-    while(run == 1)
-        msg = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        packetAmt = msg.size
-        totalACKs = 0
-        while(totalACKs < packetAmt)
-            window = fillWindow(ip, totalACKs, msg, wSize)
-            puts "Sending packets #{totalACKs} to #{totalACKs + wSize - 1}"
-            sendWindow(networkIP, window, client)
-            totalACKs = getACKs(client, wSize, totalACKs)
-			if packetAmt - totalACKs < wSize
-				wSize = packetAmt - totalACKs
-			end
+while(run == 1)
+    if(option.to_i == 0)
+        puts "Where do you want to send?"
+        ip = gets.chomp
+
+        while(!=0)
+            msg = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+            packetAmt = msg.size
+            totalACKs = 0
+            while(totalACKs < packetAmt)
+                window = fillWindow(ip, totalACKs, msg, wSize)
+                puts "Sending packets #{totalACKs} to #{totalACKs + wSize - 1}"
+                sendWindow(networkIP, window, client)
+                totalACKs = getACKs(client, wSize, totalACKs)
+    			if packetAmt - totalACKs < wSize
+    				wSize = packetAmt - totalACKs
+    			end
+            end
+            if(totalACKs == packetAmt)
+                sendPacket(client, $port, makePacket(ip, $local_ip, 2, 0, 0, ""), networkIP)
+                puts "EOT packet sent"
+                break
+            end
         end
-        if(totalACKs == packetAmt)
-            sendPacket(client, $port, makePacket(ip, $local_ip, 2, 0, 0, ""), networkIP)
-            puts "EOT packet sent"
-            run = 0
-        end
-    end
-else
-    result = ""
-    expected_seqnum = 0
-    init_packet = 0
-    while(run == 1)
-        if(init_packet == 0) 
-            packet = getPacket(client)
-            init_packet = 1
-        else
-            begin
-                Timeout.timeout(10) do
-                    packet = getPacket(client)
+    else
+        result = ""
+        expected_seqnum = 0
+        init_packet = 0
+        while(1!=0)
+            if(init_packet == 0) 
+                packet = getPacket(client)
+                init_packet = 1
+            else
+                begin
+                    Timeout.timeout(10) do
+                        packet = getPacket(client)
+                    end
+                rescue Timeout::Error
+                    puts "Did not receive any packets in 10 seconds. Assuming disconnection or lost EOT"
+                    break
                 end
-            rescue Timeout::Error
-                run = 0
-                puts "Did not receive any packets in 10 seconds. Assuming disconnection or lost EOT"
             end
-        end
 
-        if packet.type == 2
-            puts "Received EOT from #{packet.src_ip}"
-            ack = makePacket(packet.src_ip, $local_ip, 2, 1, 1, "Received EOT")
-            sendPacket(client, $port, ack, networkIP)
-            run = 0
-        else
-            puts "Received packet #{packet.seqNum}: #{packet.data} from #{packet.src_ip}"
-            ack = makePacket(packet.src_ip, $local_ip, 0, packet.seqNum, packet.seqNum + 1, "ACK")
-            sendPacket(client, $port, ack, networkIP)
-            if packet.seqNum == expected_seqnum
-                result << packet.data << " "
-                expected_seqnum += 1
+            if packet.type == 2
+                puts "Received EOT from #{packet.src_ip}"
+                ack = makePacket(packet.src_ip, $local_ip, 2, 1, 1, "Received EOT")
+                sendPacket(client, $port, ack, networkIP)
+                break
+            else
+                puts "Received packet #{packet.seqNum}: #{packet.data} from #{packet.src_ip}"
+                ack = makePacket(packet.src_ip, $local_ip, 0, packet.seqNum, packet.seqNum + 1, "ACK")
+                sendPacket(client, $port, ack, networkIP)
+                if packet.seqNum == expected_seqnum
+                    result << packet.data << " "
+                    expected_seqnum += 1
+                end
             end
         end
+        puts result
     end
-    puts result
 end
