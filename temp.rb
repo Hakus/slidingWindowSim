af0a5ca37f7c39f5110551f3b6e0a6f9ba498d59
Hakus <archeid@gmail.com>
2014-11-30 17:59:02 -0800

    Fixed packet name

--- temp.rb
+++ temp.rb
@@ -32,7 +32,7 @@ if(state.to_i == 0)
                 begin
                     Timeout.timeout(1) do
                     ack = getPacket(client)
-                    if packet.seqNum == totalACKs
+                    if ack.seqNum == totalACKs
                         puts "Received ACK (type = #{ack.type}) response from #{ack.src_ip}"
                         totalACKs += 1
                         windowACKs += 1
